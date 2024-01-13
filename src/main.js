d7fb7280686f2052e5513ce2abbfbb8baf31b207
Oleksandr-Gab <120855728+Oleksandr-Gab@users.noreply.github.com>
2024-01-13 03:48:52 +0200

    проміжний

--- src/main.js
+++ src/main.js
@@ -2,90 +2,138 @@ import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import axios from 'axios';
+
+
+const api = axios.create({
+  baseURL: 'https://pixabay.com/api/',
+  params: {
+    key: "41686068-f8a1ddec694d7ca7a5960473b",
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+
+  }
+});
+
+
 
 const gallery = document.querySelector(".gallery");
 const searchForm = document.querySelector(".search-form");
 const loading = document.querySelector(".loading");
+const btnLoad = document.querySelector(".btn-load")
+const loadMoreSpinner = document.querySelector('.load-more-spinner')
 
-loading.style.display = 'none';
 
-const searchParamsDefaults = {
-    key: '41686068-f8a1ddec694d7ca7a5960473b',
-    q: '',
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-  };
-  
-  const lightbox = new SimpleLightbox('.gallery a', {
-    nav: true,
-    captionDelay: 250,
-    captionsData: 'alt',
-    close: true,
-    enableKeyboard: true,
-    docClose: true,
-  });
+function renderHits(hits = []) {
+  const renderImg = hits.reduce((html, {largeImageURL, webformatURL, tags, likes, views, comments, downloads}) => {
+      return (
+        html +
+        `<li class="gallery-item">
+    <a href=${largeImageURL}> 
+      <img class="gallery-img" src =${webformatURL} alt=${tags}/>
+    </a>
+    <div class="gallery-text-box">
+      <p>Likes: <span class="text-value">${likes}</span></p>
+      <p>views: <span class="text-value">${views}</span></p>
+      <p>comments: <span class="text-value">${comments}</span></p>
+      <p>downloads: <span class="text-value">${downloads}</span></p>
+  </div>
+  </li>`
+      );
+    }, '');  
+    
+  gallery.insertAdjacentHTML("beforeend", renderImg);
+  lightbox.refresh();
+}
 
-  const searchImg = (params) => {
-    return fetch(`https://pixabay.com/api/?${params}`)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(response.statusText);
-        }
-        return response.json();
-      })
-      .then(({ hits }) => {
-        if (hits.length > 0) {
-          const renderImg = hits.reduce((html, hit) => {
-            return (
-              html +
-              `<li class="gallery-item">
-          <a href=${hit.largeImageURL}> 
-            <img class="gallery-img" src =${hit.webformatURL} alt=${hit.tags}/>
-          </a>
-          <div class="gallery-text-box">
-            <p>Likes: <span class="text-value">${hit.likes}</span></p>
-            <p>views: <span class="text-value">${hit.views}</span></p>
-            <p>comments: <span class="text-value">${hit.comments}</span></p>
-            <p>downloads: <span class="text-value">${hit.downloads}</span></p>
-        </div>
-        </li>`
-            );
-          }, '');
-  
-          gallery.innerHTML = renderImg;
-  
-          lightbox.refresh();
-        } else {
-          iziToast.error({
-            position: 'topRight',
-            message:
-              'Sorry, there are no images matching your search query. Please try again!',
-          });
-        }
-      })
-      .catch(error => {
-        console.log(error.message);
-      })
-      .finally(() => {
-        loading.style.display = 'none';
-      });
-  }
-  
-  searchForm.addEventListener('submit', event => {
-    event.preventDefault();
-    gallery.innerHTML = '';
-    searchParamsDefaults.q = event.target.elements.search.value.trim();
-    if (!searchParamsDefaults.q) {
-      iziToast.warning({
-        position: 'topRight',
-        message:
-          'Please, fill in the field!',
-      });
-      return;
+const lightbox = new SimpleLightbox('.gallery a', {
+  nav: true,
+  captionDelay: 250,
+  captionsData: 'alt',
+  close: true,
+  enableKeyboard: true,
+  docClose: true,
+});
+
+
+const getHits = async (params) => {
+  try {  
+    const response = await api.get("", { params });
+    return response.data;
+  } catch(error) {
+    console.error(error);
+  } 
+}
+
+const createGetHitsRequest = (q) => {
+  let page = 1;
+  let isLastPage = false;
+  const per_page = 3;
+
+  return async () => {
+    try {
+      if (isLastPage) return [];
+
+      const { hits, totalHits } = await getHits({ page, per_page, q });
+      
+      if (page >= Math.ceil(totalHits / per_page)) {
+        isLastPage = true;
+      }
+
+      page++;
+
+      return hits;
+    } catch(error) {
+      console.error(error);
     }
-    loading.style.display = 'block';
-    const searchParams = new URLSearchParams(searchParamsDefaults);
-    searchImg(searchParams);
-    event.currentTarget.reset();
+  };
+}
+
+
+let doFetch = null;
+
+searchForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  if (doFetch != null) {
+    btnLoad.removeEventListener("click", doFetch);
+    btnLoad.classList.add("is-hidden");
+    doFetch = null;
+  }
+
+  const data = new FormData(event.currentTarget);
+  const search = data.get("search");
+
+  gallery.innerHTML = "";
+
+  const fetchHits = createGetHitsRequest(search);
+
+  doFetch = async () => {
+    const articles = await makePromiseWithSpinner({
+      promise: fetchHits,
+      spinner: loadMoreSpinner
+    })
+
+    renderHits(articles);
+  }
+
+  await makePromiseWithSpinner({
+    promise: doFetch,
+    spinner: loadMoreSpinner
   });
+
+  btnLoad.classList.remove("is-hidden");
+  btnLoad.addEventListener('click', doFetch);
+});
+
+
+const makePromiseWithSpinner = async ({ promise, spinner, className = 'is-hidden' }) => {
+  spinner.classList.remove(className);
+
+  const response = await promise();
+
+  spinner.classList.add(className);
+
+  return response;
+};
